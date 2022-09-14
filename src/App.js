64e865ab8135c214c4c75c117e73ca386a639472
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-09-13 19:26:40 -0500

    browser router routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './App.css';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
